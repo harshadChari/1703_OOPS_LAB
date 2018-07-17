a8b8ce0a6b13575b4dc2087a8cf0c5621a5a9245
harshadChari <harsh.aybara@gmail.com>
2018-07-17 14:40:21 +0530

    factorial in main

--- Factorial/src/Factorial_main.java
+++ Factorial/src/Factorial_main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class Factorial_main {
 
+	static Scanner in;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number;
+		int factorial = 1;
+		in = new Scanner(System.in);
+		
+		System.out.println("Enter Number:");
+		number = in.nextInt();
+		
+		int i;
+		for(i=1;i<=number;i++)
+		{
+			factorial*=i;
+		}
+		
+		System.out.println(number + "! = " + factorial);
 
 	}
 
