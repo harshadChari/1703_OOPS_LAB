0474d79ca4b8a204ab876bb2e39ceda901eb9060
harshadChari <harsh.aybara@gmail.com>
2018-07-17 14:53:20 +0530

    factorial with class

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -1,4 +1,35 @@
 
 public class Factorial {
+	public Factorial() {
+		super();
+		System.out.println("creating object ...");
+	}
+
+
+
+	private int value;	
+	
+	public int getValue() {
+		return value;
+	}
+
+
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	
+
+
+
+	public void calcFactorial(int num) {
+		int i;
+		value = 1;
+		for(i=1;i<=num;i++)
+		{
+			value*=i;
+		}
+	}
 
 }
--- Factorial/src/Factorial_main.java
+++ Factorial/src/Factorial_main.java
@@ -4,9 +4,28 @@ public class Factorial_main {
 
 	static Scanner in;
 	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		System.out.println("Choose type:");
+		System.out.println("1:Simple");
+		System.out.println("2: Class");
+		
+		int ch = in.nextInt();
+		switch(ch)
+		{
+		case 1:
+			simpleFactorial();
+			break;
+		case 2:
+			classFactorial();
+			break;	
+		}
+
+	}
+	
+	public static void simpleFactorial() {
 		int number;
 		int factorial = 1;
-		in = new Scanner(System.in);
+		
 		
 		System.out.println("Enter Number:");
 		number = in.nextInt();
@@ -18,7 +37,19 @@ public class Factorial_main {
 		}
 		
 		System.out.println(number + "! = " + factorial);
-
+	}
+	
+	public static void classFactorial() {
+		
+		System.out.println("Enter Number:");
+		int number = in.nextInt();
+		
+		Factorial fact = new Factorial();
+		fact.calcFactorial(number);
+		
+		System.out.println(number + "! = " + fact.getValue());
+		
+		
 	}
 
 }
